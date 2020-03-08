6f825a5578aeed9faf34824ff3f8ec2cde64b6fc
pisey <pisey.sukadigital@gmail.com>
2020-03-08 12:42:40 +0700

    add secne

---
+++ Scene.swift
@@ -0,0 +1,14 @@
+//
+//  Scene.swift
+//  arkit-bird
+//
+//  Created by kasoma on 3/8/20.
+//  Copyright © 2020 kasoma. All rights reserved.
+//
+
+import SpriteKit
+import ARKit
+
+class Scene: SKScene {
+    
+}
--- ViewController.swift
+++ ViewController.swift
@@ -8,18 +8,20 @@
 
 import UIKit
 import RealityKit
+import SpriteKit
+import ARKit
 
 class ViewController: UIViewController {
+    @IBOutlet var sceneView: ARSKView!
     
     @IBOutlet var arView: ARView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        if let scene = SKScene(fileNamed: "Scene"){
+            sceneView.presentScene(scene)
+        } 
+       
     }
 }
