95ed524fa0df14fe9765acf1d2383f63381e1543
pisey <pisey.sukadigital@gmail.com>
2020-03-08 11:45:18 +0700

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  arkit-bird
+//
+//  Created by kasoma on 3/8/20.
+//  Copyright © 2020 kasoma. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
